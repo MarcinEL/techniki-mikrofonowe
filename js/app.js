2be097eadeec65b0713311c3bf85d2a14a317456
Marcin Pawel Lewandowski <m.p.lewandowski@elka.pw.edu.pl>
2020-04-07 00:42:47 +0200

    js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,31 @@
 'use strict';
 
 // Create an instance
-var wavesurfer;
+var wavesurfer = {};
 
-// Init & load
+// Init & load audio file
 document.addEventListener('DOMContentLoaded', function() {
-    // var playButton = document.querySelector('#playBtn'),
-    //     toggleMuteButton = document.querySelector('#toggleMuteBtn'),
-    //     setMuteOnButton = document.querySelector('#setMuteOnBtn'),
-    //     setMuteOffButton = document.querySelector('#setMuteOffBtn');
-
-    // Init wavesurfer
     wavesurfer = WaveSurfer.create({
-        container: '#waveform',
-        waveColor: 'black'
+        container: document.querySelector('#waveform'),
+        waveColor: '#D9DCFF',
+        progressColor: '#4353FF',
+        cursorColor: '#4353FF',
+        barWidth: 3,
+        barRadius: 3,
+        cursorWidth: 1,
+        height: 200,
+        barGap: 3
     });
 
     wavesurfer.on('error', function(e) {
         console.warn(e);
     });
 
-    // wavesurfer.once('ready', function() {
-    //     playButton.onclick = function() {
-    //         wavesurfer.playPause();
-    //     };
-
-    //     toggleMuteButton.onclick = function() {
-    //         wavesurfer.toggleMute();
-    //     };
-
-    //     setMuteOnButton.onclick = function() {
-    //         wavesurfer.setMute(true);
-    //     };
-
-    //     setMuteOffButton.onclick = function() {
-    //         wavesurfer.setMute(false);
-    //     };
-    // });
-
+    // Load audio from URL
     wavesurfer.load('https://ia800301.us.archive.org/15/items/fire_and_ice_librivox/fire_and_ice_frost_apc_64kb.mp3');
+
+    // Play button
+    var button = document.querySelector('[data-action="play"]');
+
+    button.addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
 });
