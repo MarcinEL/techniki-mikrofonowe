08272c6a9c6f4991a62cdfaabc7c9275187ab1f9
Marcin Pawel Lewandowski <m.p.lewandowski@elka.pw.edu.pl>
2020-04-07 00:16:03 +0200

    js

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ var wavesurfer;
 
 // Init & load
 document.addEventListener('DOMContentLoaded', function() {
-    var playButton = document.querySelector('#playBtn'),
-        toggleMuteButton = document.querySelector('#toggleMuteBtn'),
-        setMuteOnButton = document.querySelector('#setMuteOnBtn'),
-        setMuteOffButton = document.querySelector('#setMuteOffBtn');
+    // var playButton = document.querySelector('#playBtn'),
+    //     toggleMuteButton = document.querySelector('#toggleMuteBtn'),
+    //     setMuteOnButton = document.querySelector('#setMuteOnBtn'),
+    //     setMuteOffButton = document.querySelector('#setMuteOffBtn');
 
     // Init wavesurfer
     wavesurfer = WaveSurfer.create({
@@ -20,23 +20,23 @@ document.addEventListener('DOMContentLoaded', function() {
         console.warn(e);
     });
 
-    wavesurfer.once('ready', function() {
-        playButton.onclick = function() {
-            wavesurfer.playPause();
-        };
+    // wavesurfer.once('ready', function() {
+    //     playButton.onclick = function() {
+    //         wavesurfer.playPause();
+    //     };
 
-        toggleMuteButton.onclick = function() {
-            wavesurfer.toggleMute();
-        };
+    //     toggleMuteButton.onclick = function() {
+    //         wavesurfer.toggleMute();
+    //     };
 
-        setMuteOnButton.onclick = function() {
-            wavesurfer.setMute(true);
-        };
+    //     setMuteOnButton.onclick = function() {
+    //         wavesurfer.setMute(true);
+    //     };
 
-        setMuteOffButton.onclick = function() {
-            wavesurfer.setMute(false);
-        };
-    });
+    //     setMuteOffButton.onclick = function() {
+    //         wavesurfer.setMute(false);
+    //     };
+    // });
 
     wavesurfer.load('https://ia800301.us.archive.org/15/items/fire_and_ice_librivox/fire_and_ice_frost_apc_64kb.mp3');
 });
